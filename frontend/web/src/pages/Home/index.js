3462873bc8522b922e5762f39338e1664c19fd0d
anathik <me@anathi.io>
2017-10-19 20:45:37 -0400

    started on the next sections

--- frontend/web/src/pages/Home/index.js
+++ frontend/web/src/pages/Home/index.js
@@ -1,65 +1,115 @@
 import React, { Component } from "react";
 import scrollToComponent from "react-scroll-to-component";
-import "./index.css";
+import "./intro.css";
+import "./about-me.css";
 
-import backgroundDiv1 from "../../media_assets/nyc3.jpg";
+import backgroundDiv1 from "../../media_assets/nyc4.jpg";
 import anathiLogo from "../../media_assets/anathiIconRedSquare.jpg";
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      visible: false
+    };
+  }
+
+  showMenuOnClick() {
+    if (this.state.visible === false) {
+      this.setState({ visible: true });
+    }
+    if (this.state.visible === true) {
+      this.setState({ visible: false });
+    }
+  }
+
+  closeMenuOnClick() {
+    if (this.state.visible === true) {
+      this.setState({ visible: false });
+    }
+  }
+
+  closeMenuScrollOverride() {
+    if (this.state.visible === true) {
+      console.log("Hovered!");
+      this.setState({ visible: false });
+    }
+    if (this.state.visible === false) {
+      console.log("Not hovered anymore!");
+      scrollToComponent(document.getElementById("about-me-div"), {
+        offset: 0,
+        align: "top",
+        duration: 700
+      });
+    }
+  }
+
   render() {
+    let pageMenuClassName = "page-menu";
+
+    let pageDivOverlayClassName = "page-div-overlay";
+    let splashGrettingClassName = "greeting";
+    let splashTitleClassName = "title";
+    let pageLogoClassName = "page-logo";
+    let pageLogoContainerClassName = "page-logo-container";
+    let scrollDivClassName = "scrollTo-button-container";
+
+    if (this.state.visible === true) {
+      pageMenuClassName += "-visible";
+
+      pageDivOverlayClassName += "-dimmed";
+      splashGrettingClassName += "-dimmed";
+      splashTitleClassName += "-dimmed";
+      pageLogoClassName += "-dimmed";
+      pageLogoContainerClassName += "-dimmed";
+      scrollDivClassName += "-dimmed";
+    }
+
     return (
       <div className="Home">
-        {/* This is the first page length. Welcome */}
+        {/* Splash Div */}
         <div
           className="page-div"
           id="splash-div"
           style={{ backgroundImage: `url(${backgroundDiv1})` }}
         >
-          <div className="page-div-background-overlay">
-            <div className="menu-bar">
-              <div className="menu-button">
-                <i className="material-icons" id="menu-icon">
-                  &#xE5D2;
-                </i>
-              </div>
-              <div className="page-menu-items">
-                <div className="back-to-top-button-container">
-                  <div
-                    className="back-to-top-button"
-                    onClick={() =>
-                      scrollToComponent(document.getElementById("splash-div"), {
-                        offset: 0,
-                        align: "top",
-                        duration: 500
-                      })}
-                  >
-                    <div className="back-to-top-button-text">Back to Top</div>
-                  </div>
-                </div>
-              </div>
-              <div className="menu-div-view" />
+          <div className={pageDivOverlayClassName}>
+            <div
+              className="menu-button"
+              onClick={this.showMenuOnClick.bind(this)}
+            >
+              <i className="material-icons" id="menu-icon">
+                &#xE5D2;
+              </i>
             </div>
-            <div id="page-logo-container">
+            <div className="menu-selection">
+              <div className="menu-selection-intro" />
+              <div className="menu-line" />
+              <div className="menu-selection-aboutUs" />
+              <div className="menu-line" />
+            </div>
+            <div
+              className={pageLogoContainerClassName}
+              onClick={this.closeMenuOnClick.bind(this)}
+            >
               <div
-                id="page-logo"
+                className={pageLogoClassName}
+                onClick={this.closeMenuOnClick.bind(this)}
                 style={{
                   backgroundImage: `url(${anathiLogo})`
                 }}
               />
             </div>
-            <div id="greeting">Hello. I'm Anathi</div>
-            <div id="title">Frontend Software Developer</div>
-            <div className="scrollTo-button-container">
-              <div
-                className="scrollTo-about-button"
-                onClick={() =>
-                  scrollToComponent(document.getElementById("about-me-div"), {
-                    offset: 0,
-                    align: "top",
-                    duration: 500
-                  })}
-              >
-                <div className="scrollTo-text">About Me</div>
+            <div className={splashGrettingClassName}>Hello. I'm Anathi</div>
+            <div className={splashTitleClassName}>
+              Frontend Software Developer
+            </div>
+            <div
+              className={scrollDivClassName}
+              onClick={this.closeMenuScrollOverride.bind(this)}
+            >
+              <div className="scrollTo-about-button">
+                <div className="scrollTo-text">Scroll Down</div>
                 <i className="material-icons" id="scrollTo-icon">
                   &#xE313;
                 </i>
@@ -68,6 +118,12 @@ class Home extends Component {
           </div>
         </div>
         <div className="page-div" id="about-me-div" />
+        <div className="about-me-page-divider">
+          <div className="about-me-page-divider-right">
+            <div className="about-me-mosaic">{/* Top right */}</div>
+          </div>
+          <div className="about-me-page-divider-left" />
+        </div>
         <div className="page-div" id="skills-div" />
         <div className="page-div" id="projects-div" />
         <div className="page-div" id="activities-div" />
